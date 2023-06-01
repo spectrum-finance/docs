f0f26d8e1555a2127745d604e5c661b318974827
yashablack <iamyashablack@gmail.com>
2023-06-01 17:42:43 +0300

    feat: add socials links

--- src/components/HomepageSocials/HomepageSocials.tsx
+++ src/components/HomepageSocials/HomepageSocials.tsx
@@ -1,55 +1,91 @@
 import React from 'react';
-import clsx from 'clsx';
-import styles from './HomepageSocials.module.css';
-import Link from "@docusaurus/core/lib/client/exports/Link";
-import ExternalArrow from "../../../static/icons/external-arrow.svg";
+import styled from "@emotion/styled";
+import {Row} from "@site/src/components/Row/Row";
+
+import TwitterLogo from '@site/static/icons/twitter-mark.svg'
+import TelegramLogo from '@site/static/icons/telegram-mark.svg'
+import DiscordLogo from '@site/static/icons/discord-mark.svg'
+
 
 const socialsData = [
-   {
-        title: 'Twitter',
-        link:  'https://twitter.com/spectrumlabs_',
-        Icon:  require('../../../static/icons/twitter-mark.svg').default,
-        description: 'Latest project status notifications!',
-   },
-   {
-        title: 'Telegram',
-        link: 'https://t.me/spectrum_labs_community',
-        Icon:  require('../../../static/icons/telegram-mark.svg').default,
-        description: 'Get more info about Spectrum Finance protocol.',
-   },
-   {
+    {
         title: 'Discord',
-        link: 'https://discord.com/invite/zY2gmTYQVD',
-        Icon:  require('../../../static/icons/discord-mark.svg').default,
-        description: 'Discuss features, get support or contact devs.',
+        href: 'https://discord.com/invite/zY2gmTYQVD',
+        Icon:  DiscordLogo,
+        content: 'Join our Discord Community',
     },
     {
-        title: 'Reddit',
-        link: 'https://www.reddit.com/r/SpectrumLabs/',
-        Icon:  require('../../../static/icons/reddit-mark.svg').default,
-        description: 'Enjoy the subreddit community',
+        title: 'Twitter',
+        href:  'https://twitter.com/spectrumlabs_',
+        Icon:  TwitterLogo,
+        content: 'Stay informed about the latest updates',
     },
+    {
+        title: 'Telegram',
+        href: 'https://t.me/spectrum_labs_community',
+        Icon:  TelegramLogo,
+        content: 'Join our Telegram Community',
+    },
+    // {
+    //     title: 'Reddit',
+    //     href: 'https://www.reddit.com/r/SpectrumLabs/',
+    //     Icon:  require('../../../static/icons/reddit-mark.svg').default,
+    //     content: 'Enjoy the subreddit community',
+    // },
 ];
 
-const SocialsTile = ({Icon, title, description, link}) => {
-  return (
-    <Link sensitive={false} href={link} target="_blank" className={clsx('col col--4', styles.social)}>
-      <div className={clsx('text--center', 'padding-horiz--md', styles.wrapper)}>
-        <ExternalArrow className={styles.arrow} />
-        <h3>{title}</h3>
-        <Icon style={{ width: 100, height: 100, margin: '0 auto' }} />
-        <p>{description}</p>
-      </div>
-    </Link>
-  );
+
+const SocialsTileStyled = styled.a`
+  cursor: pointer;
+  border: 1px solid var(--ifm-toc-border-color);
+  border-radius: var(--ifm-border-radius);
+  padding: 1rem;
+  transition: all .2s ease-in-out;
+
+  p, h3 {
+    margin: 0;
+  }
+
+  .social-icon {
+    height: 96px;
+    width: 96px;
+    margin-right: 16px;
+  }
+
+  &:hover {
+    text-decoration: none;
+    transform: scale(1.02);
+  }
+`
+
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const SocialsTile = ({Icon, title, content, href}) => {
+    return (
+        <SocialsTileStyled href={href} target="_blank">
+            <Flex>
+                <Icon className="social-icon" />
+                <div>
+                    <h3>{title}</h3>
+                    <p>{content}</p>
+                </div>
+            </Flex>
+        </SocialsTileStyled>
+    )
 }
 
 export const HomepageSocials = () => {
   return (
-    <section className={styles.socials}>
-        {socialsData.map((props, idx) => (
-            <SocialsTile key={idx} {...props} />
-        ))}
+    <section>
+        <Row>
+            {socialsData.map((props, idx) => (
+                <SocialsTile key={idx} {...props} />
+            ))}
+        </Row>
+
     </section>
   );
 }
